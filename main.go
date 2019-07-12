de7cb71c32b24174cb89100ae83a8ce72fc8b4f6
Lindsay <lindsay@Lindsays-MacBook-Pro.local>
2019-07-12 16:40:24 +1200

    update

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "log"
 
+func main() {
+	t1 := &square{}
+	t2 := &square{}
+	t1.calculate(4)
+	t2.calculate(8)
+	log.Println(t1.getResult())
+	log.Println(t2.getResult())
+}
+
+type square struct {
+	origin int64
+	result int64
+}
+
+func (s *square) calculate(value int64) {
+	s.origin = value
+	s.result = value * value
+}
+
+func (s *square) getResult() int64 {
+	return s.result
 }
